07b72be8a60522cefcd805b1f53882ebaec615a4
kv-2 <str_token@mail.ru>
2019-08-01 20:01:54 +0300

    update shema

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-const {
-  ApolloServer
-} = require('apollo-server')
+const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
-`
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
+  }
+`;
+var _id = 0;
+var photos = [];
 const resolvers = {
   Query: {
-    totalPhotos: () => 54
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args
+      };
+      photos.push(newPhoto);
+      return newPhoto;
+    }
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
-})
+});
 
 server
   .listen()
-  .then(({
-    url
-  }) => console.log(`GraphQL servise running on ${url}`))
+  .then(({ url }) => console.log(`GraphQL servise running on ${url}`));
