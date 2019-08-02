602b565e42907b42de89f357a3fda474c7d19cab
kv-2 <str_token@mail.ru>
2019-08-02 17:22:02 +0300

    update shema, add one to meny - iser to photos

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -14,6 +20,7 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
   }
   type Query {
     totalPhotos: Int!
@@ -29,7 +36,42 @@ const typeDefs = `
   }
 `;
 var _id = 0;
-var photos = [];
+var users = [
+  {
+    githubLogin: "IvanDurov",
+    name: "Izya Groisman"
+  },
+  {
+    githubLogin: "MrTwister",
+    name: "Abram Abramov"
+  },
+  {
+    githubLogin: "MamkeyMan",
+    name: "Alex Ogurcov"
+  }
+];
+var photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "Lorem upsum something",
+    category: "ACTION",
+    githubUser: "IvanDurov"
+  },
+  {
+    id: "2",
+    name: "Enjoying the banana",
+    category: "SELFIE",
+    githubUser: "MamkeyMan"
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "I and my treasures",
+    category: "LANDSCAPE",
+    githubUser: "MrTwister"
+  }
+];
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -46,7 +88,15 @@ const resolvers = {
     }
   },
   Photo: {
-    url: parent => `http://yousete.com/img/${parent.id}.jpg`
+    url: parent => `http://yousete.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser);
+    }
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin);
+    }
   }
 };
 
