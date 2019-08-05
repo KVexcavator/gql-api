5d60e6a8671023abd6ab750e808f876f0dba541e
kv-2 <str_token@mail.ru>
2019-08-05 14:44:36 +0300

    out dates

--- index.js
+++ index.js
@@ -15,62 +15,6 @@ const {
 require('dotenv').config();
 
 
-var _id = 0;
-var users = [{
-    githubLogin: "IvanDurov",
-    name: "Izya Groisman"
-  },
-  {
-    githubLogin: "MrTwister",
-    name: "Abram Abramov"
-  },
-  {
-    githubLogin: "MamkeyMan",
-    name: "Alex Ogurcov"
-  }
-];
-var photos = [{
-    id: "1",
-    name: "First",
-    description: "Lorem upsum something",
-    category: "ACTION",
-    githubUser: "IvanDurov",
-    created: "10-19-1971"
-  },
-  {
-    id: "2",
-    name: "Second",
-    category: "SELFIE",
-    githubUser: "MamkeyMan",
-    created: "5-9-2001"
-  },
-  {
-    id: "3",
-    name: "The end",
-    description: "I and my treasures",
-    category: "LANDSCAPE",
-    githubUser: "MrTwister",
-    created: "2-05-2019"
-  }
-];
-var tags = [{
-    photoID: "1",
-    userID: "MrTwister"
-  },
-  {
-    photoID: "2",
-    userID: "MamkeyMan"
-  },
-  {
-    photoID: "2",
-    userID: "IvanDurov"
-  },
-  {
-    photoID: "2",
-    userID: "MrTwister"
-  }
-];
-
 async function start() {
   const app = express();
   const MONGO_DB = process.env.DB_HOST;
