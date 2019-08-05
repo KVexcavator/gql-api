a243eae3c947973d0f45a466cb156826d586574f
kv-2 <str_token@mail.ru>
2019-08-05 17:05:57 +0300

    add gitgub

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,5 +1,5 @@
 module.exports = {
-  postPhoto(parent, args) {
+  async postPhoto(parent, args) {
     var newPhoto = {
       id: _id++,
       ...args.input,
@@ -7,5 +7,51 @@ module.exports = {
     };
     photos.push(newPhoto);
     return newPhoto;
+  },
+
+  async githubAuth(parent, {
+    code
+  }, {
+    db
+  }) {
+
+    let {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+
+    if (message) {
+      throw new Error(message)
+    }
+
+    let latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url
+    }
+
+    const {
+      ops: [user]
+    } = await db
+      .collection('users')
+      .replaceOne({
+        githubLogin: login
+      }, latestUserInfo, {
+        upsert: true
+      })
+
+    return {
+      user,
+      token: access_token
+    }
+
   }
 };
