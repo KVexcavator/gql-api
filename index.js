e8f2ae1ffebbdd84073c3b2d144404ab1233c079
kv-2 <str_token@mail.ru>
2019-08-05 15:45:37 +0300

    add request function github account

--- index.js
+++ index.js
@@ -56,3 +56,35 @@ async function start() {
 }
 
 start();
+
+const requestGithubToken = credentials =>
+  fetch(
+    'https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'applycation/json'
+      },
+      body: JSON.stringify(credentials)
+    }
+  )
+  .then(res => res.json())
+  .catch(error => {
+    throw new Error(JSON.stringify(error))
+  });
+
+const requestGithubUserAccount = token =>
+  fetch('https://api.github.com/user?access_token=${token}')
+  .then(toJSON)
+  .catch(throwError);
+
+async authorizeWithGithub(credentials) {
+  const {
+    access_token
+  } = await requestGithubToken(credentials)
+  const githubUser = await requestGithubUserAccount(access_token)
+  return {
+    ...githubUser,
+    access_token
+  }
+}
